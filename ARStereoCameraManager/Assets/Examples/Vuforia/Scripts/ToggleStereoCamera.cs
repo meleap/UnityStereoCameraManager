dabcc421bf191b8f6bd73c1f8e3b187264d24aa8
trapple <trapplejp@gmail.com>
2020-10-05 17:30:21 +0900

    Asset切り替え実験用

---
+++ ARStereoCameraManager/Assets/Examples/Vuforia/Scripts/ToggleStereoCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class ToggleStereoCamera : MonoBehaviour
+{
+    public Button toggleURP;
+
+    public RenderPipelineAsset DefaultPipelineAsset;
+    public RenderPipelineAsset StereoCameraPipelineAsset;
+
+    bool isStereo = false;
+
+    void Start()
+    {
+        toggleURP.onClick.AddListener(Toggle);
+
+    }
+
+    void Toggle()
+    {
+        GraphicsSettings.renderPipelineAsset = isStereo ? DefaultPipelineAsset : StereoCameraPipelineAsset;
+        isStereo = !isStereo;
+    }
+}
